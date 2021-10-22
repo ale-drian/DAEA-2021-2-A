5241c853172da487136a0f46f018684461755866
Alejandra Adrian Tejada <thalia.adrian@tecsup.edu.pe>
2021-10-22 13:48:48 -0500

    Listar productos - ( g )

---
+++ lab10/Lab10/Lab10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //origen de datos
+            NorthwndDataContext context = new NorthwndDataContext();
+
+            //Creacion de la consulta
+            var query = from p in context.Products
+                        select p;
+
+            //ejecucion de la consulta
+            foreach (var prod in query) {
+                Console.WriteLine("ID={0} \t Name={1}", prod.ProductID, prod.ProductName);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
