f17938724c36b5636379cf9841a2828720fc5fc1
Alejandra Adrian Tejada <thalia.adrian@tecsup.edu.pe>
2021-11-30 09:51:45 -0500

    Listar

---
+++ Practica4/Practica4/Controllers/PrestamosController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practica4.Models;
+using System.Net;
+using System.Data.Entity;
+
+namespace Practica4.Controllers
+{
+    public class PrestamosController : Controller
+    {
+        #region Contexto
+        // Atributo contexto
+        private bibiotecaEntities _contexto;
+        //PPropiedad contexto
+        public bibiotecaEntities Contexto
+        {
+            set { _contexto = value; }
+            get
+            {
+                if (_contexto == null)
+                    _contexto = new bibiotecaEntities();
+                return _contexto;
+            }
+        }
+        #endregion
+        // GET: Prestamos
+        public ActionResult Index()
+        {
+            return View(Contexto.Prestamos.ToList());
+        }
+    }
+}
