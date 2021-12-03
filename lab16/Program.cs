ac59c7affe387520692d1f4cfe6ce310486a2064
Alejandra Adrian Tejada <thalia.adrian@tecsup.edu.pe>
2021-12-03 14:25:01 -0500

    Person

---
+++ lab16/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+// Agregar el modelo persons
+using persons.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//Agregar el contexto
+builder.Services.AddDbContext<PersonContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("PersonDb")));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
